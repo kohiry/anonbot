de7c6f346d5123f772f11917853fcbff9f714d43
kohiry <kohiry@mail.ru>
2022-04-06 09:06:21 +0400

    writed alg finding free users and change status users

--- sort.py
+++ sort.py
@@ -1,10 +1,17 @@
 
 class Anonims:
     def __init__(self, data):
-        sef.all_user_id = data # all id
+        sef.all_user = data # all id
         self.good_data = [] # id status True
+
+    def find_good(self):
+        self.good_data = [i for i in self.all_user if i.status == True] # find free users
 
 
 class User:
-    def __init__(self, status):
+    def __init__(self, id, status=False):
         self.status = status # True/False
+        self.id = id
+
+    def change_status(self, status):
+        self.status = status
